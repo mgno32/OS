34d400ce974e8b55275794fe12249831491adb5d
hexiaoyu <hexiaoyu42@outlook.com>
2016-07-01 04:24:26 +0800

    second

--- include/chprint.h
+++ include/chprint.h
@@ -1,9 +1,12 @@
-
+int color[3] = {0x7,0x4,0x9}; 
+int printNumber = 0;
+int globalColor = 0x7;
 __attribute__((regparm(3)))
 void print(int r, int c, char ch){
 	int offset=(80 * (r) + c) * 2;
-	int co = offset % 3;
-	co = color[co];
+	int conum = printNumber % 3;
+	printNumber ++;
+	int co = color[conum];
 	asm volatile("push es;"
 			"mov es,ax;"
 			"mov es:[bx], cx;"
@@ -12,3 +15,13 @@ void print(int r, int c, char ch){
 			);
 }
 
+__attribute__((regparm(3)))
+void printColor(int r, int c, char ch){
+	int offset=(80 * (r) + c) * 2;
+	asm volatile("push es;"
+			"mov es,ax;"
+			"mov es:[bx], cx;"
+			"pop es;":
+			:"a"(0xB800),"b"(offset),"c"((globalColor << 8) | ch)
+			);
+}
--- source/pro1.c
+++ source/pro1.c
@@ -1,16 +1,18 @@
 #include<chprint.h>
-int *color = [0x7,0x4,0x9];
-char *str = 'shirley';
+char *str = "shirley";
 
 int main(){
 	int r = 0;
 	int c = 0;
 	while(1){
 		for(r = 0, c = 0; r <= 11; r++, c++){
-			char* a = 0x8B00;
-			*a = str[r % 7];
-			*(a + 1) = color[r % 3];
-			print(r,c,'a')
+			print(r,c,str[r % 7]);
+		}
+	 	for (int r = 12; r < 14;++r){
+	 		for (int c = 0; c < 5;++c){
+				print(r,c,'a'+r);
+			}
 		}
 	}
+	return 0;
 }
